ee63710ee6a18cee2ca1547f3bd0debaf7f3ec3a
chas0000 <86915826+chas0000@users.noreply.github.com>
2025-04-06 20:21:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,13 +9,13 @@ else
     echo "配置文件已存在，跳过拷贝"
 fi
 
-# 2. 检查 /app/wrapper/rootfs/data 是否为空
-if [ ! -d /app/wrapper/rootfs/data ] || [ -z "$(ls -A /app/wrapper/rootfs/data 2>/dev/null)" ]; then
-    echo "/app/wrapper/rootfs/data 是空的，拷贝数据目录"
+# 2. 检查 /app/rootfs/data 是否为空
+if [ ! -d /app/rootfs/data ] || [ -z "$(ls -A /app/rootfs/data 2>/dev/null)" ]; then
+    echo "/app/rootfs/data 是空的，拷贝数据目录"
     shopt -s dotglob  # 启用 dotglob 选项以包含隐藏文件
-    cp -r /backup/data/. /app/wrapper/rootfs/data/
+    cp -r /backup/data/. /app/rootfs/data/
 else
-    echo "/app/wrapper/rootfs/data 不为空，跳过拷贝"
+    echo "/app/rootfs/data 不为空，跳过拷贝"
 fi
 
 # 后台运行 ttyd
