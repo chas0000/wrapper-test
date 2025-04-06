fc8a8937a85fb26c517a6cb0a527a2466b39dbd0
chas0000 <86915826+chas0000@users.noreply.github.com>
2025-04-06 21:28:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ fi
 if [ ! -d /app/rootfs/data ] || [ -z "$(ls -A /app/rootfs/data 2>/dev/null)" ]; then
     echo "/app/rootfs/data 是空的，拷贝数据目录"
     shopt -s dotglob  # 启用 dotglob 选项以包含隐藏文件
-    cp -r /backup/data/. /app/rootfs/data/data/
+    cp -r /backup/rootfs/. /app/rootfs/
     ls /app/rootfs/data
 else
     echo "/app/rootfs/data 不为空，跳过拷贝"
