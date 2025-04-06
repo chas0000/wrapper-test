c544de41fd4154ac6a1c52d45693c0e05d4be187
chas0000 <86915826+chas0000@users.noreply.github.com>
2025-04-06 19:54:53 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,7 +12,8 @@ fi
 # 2. 检查 /app/wrapper/rootfs/data 是否为空
 if [ ! -d /app/wrapper/rootfs/data ] || [ -z "$(ls -A /app/wrapper/rootfs/data 2>/dev/null)" ]; then
     echo "/app/wrapper/rootfs/data 是空的，拷贝数据目录"
-    cp /backup/data/* /app/wrapper/rootfs/data/
+    shopt -s dotglob  # 启用 dotglob 选项以包含隐藏文件
+    cp -r /backup/data/* /app/wrapper/rootfs/data/
 else
     echo "/app/wrapper/rootfs/data 不为空，跳过拷贝"
 fi
