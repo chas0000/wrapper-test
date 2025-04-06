275497043d8c0fc3ee6fcf2b79a6c5bcdfe1f69c
chas0000 <86915826+chas0000@users.noreply.github.com>
2025-04-06 20:41:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# 1. 检查 /app/config.yaml 是否存在
-if [ ! -f /app/config.yaml ]; then
+# 1. 检查 /app/amdl/config.yaml 是否存在
+if [ ! -f /app/amdl/config.yaml ]; then
     echo "配置文件不存在，拷贝 config.yml 到 /app"
-    cp /backup/config.yaml /app/
-    ls /app
+    cp /backup/config.yaml /app/amdl/
+    ls /app/amdl
 else
     echo "配置文件已存在，跳过拷贝"
 fi
@@ -15,10 +15,11 @@ if [ ! -d /app/rootfs/data ] || [ -z "$(ls -A /app/rootfs/data 2>/dev/null)" ]; 
     echo "/app/rootfs/data 是空的，拷贝数据目录"
     shopt -s dotglob  # 启用 dotglob 选项以包含隐藏文件
     cp -r /backup/data/. /app/rootfs/data/
+    ls /app/rootfs/data
 else
     echo "/app/rootfs/data 不为空，跳过拷贝"
 fi
 
 # 后台运行 ttyd
-ttyd bash &
+sudo ttyd bash &
 exit 0
