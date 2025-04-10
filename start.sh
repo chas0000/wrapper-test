bfe0d28bfd872229cf0498a72f1f9dd287e95a14
chas0000 <86915826+chas0000@users.noreply.github.com>
2025-04-10 23:14:25 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,31 +1,31 @@
-FROM gpac/ubuntu
+#!/bin/bash
+set -e
 
-# 更新 apt 源并安装中文语言包
-RUN apt update && apt install -y locales
+# 1. 检查 /app/amdl/config.yaml 是否存在
+if [ ! -f /app/amdl/config.yaml ]; then
+    echo "配置文件不存在，拷贝 config.yml 到 /app"
+    cp /backup/config.yaml /app/amdl/
+    ls /app/amdl
+else
+    cp /app/amdl/config.yaml /app/
+fi
 
-# 生成中文语言包
-RUN locale-gen zh_CN.UTF-8
+# 2. 检查 /app/rootfs/data 是否为空
+if [ ! -d /app/rootfs/data ] || [ -z "$(ls -A /app/rootfs/data 2>/dev/null)" ]; then
+    echo "/app/rootfs/data 是空的，拷贝数据目录"
+    shopt -s dotglob  # 启用 dotglob 选项以包含隐藏文件
+    cp -r /backup/rootfs/. /app/rootfs/
+    ls /app/rootfs/data
+else
+    echo "/app/rootfs/data 不为空，跳过拷贝"
+fi
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
-# 设置默认的语言环境为 zh_CN.UTF-8
-# RUN update-locale LANG=zh_CN.UTF-8 LC_ALL=zh_CN.UTF-8
-
-# 安装其他依赖包
-RUN apt install -y screen ttyd
-
-WORKDIR /app
-
-COPY ./wrapper /app/
-COPY ./wrapper /backup/
-COPY ./mp4decrypt /usr/bin/
-COPY ./dl /app/
-COPY ./config.yaml /app/amdl/
-COPY ./config.yaml /backup/
-RUN ln -s /app/dl /usr/bin
-COPY ./start.sh /app/
-RUN chmod -R 755 /app && chmod 755 /usr/bin/mp4decrypt && chmod 755 /app/start.sh
-
-RUN echo 'mouse on' > /root/.screenrc
-
-ENV args ""
-
-CMD bash -c "/app/start.sh && /app/wrapper ${args}"
+stty cols 120 rows 40
+stty raw -echo
+export TERM=xterm-256color
+# 后台运行 ttyd
+ttyd -W  screen -xR mysession &
+#ttyd -W tmux new -A -s mysession &
+exit 0
